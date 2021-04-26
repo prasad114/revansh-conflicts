346fcb223175e01b7f36d2a48da2081c52b6ec13
badboy <chintalaajay123@gmail.com>
2021-04-26 19:26:52 +0530

    modified

--- app.java
+++ app.java
@@ -1,3 +1,17 @@
+Swap Two Numbers
+Find the Size of int, float, double and char
+Compute Quotient and Remainder
+Find ASCII Value of a Character
+Multiply Two Floating-Point Numbers
+Add Two Integers
+Print an Integer (Entered by the User)
+C "Hello, World!" Program
+
+Join our newsletter for the latest updates.
+Enter Email Address*
+Join
+
+C Program to Swap Two Numbers
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
