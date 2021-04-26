a33093ff11b1bcec49936f5a274e31eb5455aec9
badboy <chintalaajay123@gmail.com>
2021-04-26 19:14:16 +0530

    modified

--- app.java
+++ app.java
@@ -1,6 +1,9 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+
+
+
+
+If you use the printf() function without writing #include <stdio.h>, the program will not compile.
+The execution of a C program starts from the main() function.
+printf() is a library function to send formatted output to the screen. In this program, printf() displays Hello, World! text on the screen.
+The return 0; statement is the "Exit status" of the program. In simple terms, the program ends with this statement.
+
