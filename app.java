89f1573995dbbbc42d512cbc4c52fd94a9cfa1f6
badboy <chintalaajay123@gmail.com>
2021-04-26 20:05:55 +0530

    khjgdsj

--- app.java
+++ app.java
@@ -2,8 +2,8 @@
 
 
 
-If you use the printf() function without writing #include <stdio.h>, the program will not compile.
-The execution of a C program starts from the main() function.
+
+he execution of a C program starts from the main() function.
 printf() is a library function to send formatted output to the screen. In this program, printf() displays Hello, World! text on the screen.
 The return 0; statement is the "Exit status" of the program. In simple terms, the program ends with this statement.
 
